71ffef02da4432e7dea407b5000de30609e96130
christopher <stevens14141@gmial.com>
2020-12-11 17:29:42 -0500

    ejercicios

---
+++ promedio.js
@@ -0,0 +1,8 @@
+    let examen1 = prompt('Ingrese la nota del primer examen');
+    let examen2 = prompt ('Ingrese la nota del segundo examen');
+    let examen3 = prompt('Ingrese la nota del tercer examen');
+    let examen4 = prompt('Ingrese la nota del cuarto examen');
+
+    promedio = (parseInt (examen1) + parseInt (examen2) + parseInt (examen3) + parseInt  (examen4))
+
+    alert ('Su promedio es: ' + promedio/4)
---
+++ sumar.js
@@ -0,0 +1,11 @@
+function sumar(a, b) {
+    return a + b
+}
+
+console.log(sumar (2, 5))
+console.log(sumar (7, 3))
+console.log(sumar (8, 7))
+console.log(sumar (6, 1))
+console.log(sumar (10, 8))
+
+console.log(sumar (sumar(5, 8), sumar (7,18)))
